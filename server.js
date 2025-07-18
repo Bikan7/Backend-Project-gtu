33dd2d4d5822b9a7553a8ca8418f20dc4b2c90bb
Bikan Chowdhury <149146214+Bikan7@users.noreply.github.com>
2025-07-18 09:40:14 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const firstYearBooks = require("./data/1st-year-books.json");
 app.set("view engine", "ejs");
-app.set("views", "views")
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/1st-year-books", (req, res) => {
   res.json(firstYearBooks);
 });
 
 app.get("/test", (req, res) => {
-  res.render("index", { data:firstYearBooks });
+  res.render("index", { data: firstYearBooks });
   // console.log(typeof(firstYearBooks))
 });
 
